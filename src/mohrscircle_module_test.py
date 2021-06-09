f90f7bc403407f63b1cb88f10a4362c343c1e30b
Jake Joseph <jakejosephcs@gmail.com>
2021-06-09 19:41:27 -0400

    Add tests for constructor

---
+++ src/mohrscircle_module_test.py
@@ -0,0 +1,35 @@
+from mohrscircle_module import *
+import pytest
+
+
+def test_get_sigma_x():
+    """sigma_x of MohrsCircle(50, -10, 40) should be 50"""
+    assert MohrsCircle(50, -10, 40).get_sigma_x() == 50
+
+
+def test_get_sigma_y():
+    """sigma_y of MohrsCircle(50, -10, 40) should be -10"""
+    assert MohrsCircle(50, -10, 40).get_sigma_y() == -10
+
+
+def test_get_tau_xy():
+    """tau_xy of MohrsCircle(50, -10, 40) should be 40"""
+    assert MohrsCircle(50, -10, 40).get_tau_xy() == 40
+
+
+def test_get_sigma_x_valueError():
+    """get_sigma_x of MohrsCircle('50', -10, 40) should raise a ValueError"""
+    with pytest.raises(ValueError):
+        MohrsCircle('50', -10, 40).get_sigma_x()
+
+
+def test_get_sigma_y_valueError():
+    """get_sigma_y of MohrsCircle(50, '-10', 40) should raise a ValueError"""
+    with pytest.raises(ValueError):
+        MohrsCircle(50, '-10', 40).get_sigma_y()
+
+
+def test_get_tau_xy_valueError():
+    """tau_xy of MohrsCircle(50, -10, "40") should raise a Value error"""
+    with pytest.raises(ValueError):
+        MohrsCircle(50, -10, '40').get_tau_xy()
