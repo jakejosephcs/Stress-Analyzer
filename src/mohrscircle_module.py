d29bbc76a551a7b0d446b54622d8662f00187132
Jake Joseph <jakejosephcs@gmail.com>
2021-06-10 16:01:56 -0400

    Add functions skeleton

---
+++ src/mohrscircle_module.py
@@ -0,0 +1,40 @@
+class MohrsCircle():
+    def __init__(self, sigma_x, sigma_y, tau_xy):
+        """constructor"""
+        return ""
+
+    def get_sigma_x(self):
+        """get_sigma_x returns the normal stress in the x-direction."""
+        return ""
+
+    def get_sigma_y(self):
+        """get_sigma_y returns the normal stress in the x-direction."""
+        return ""
+
+    def get_tau_xy(self):
+        """get_tau_xy returns the shear stress in the xy-direction."""
+        return ""
+
+    def sigma_avg(self):
+        """sigma_avg returns the average normal stress (the center of mohr's circle)"""
+        return ""
+
+    def tau_max_xy(self):
+        """tau_max_xy returns the max shear stress (radius of mohr's circle)"""
+        return ""
+
+    def max_principal_stress(self):
+        """max_principal_stress returns the max principal stress"""
+        return ""
+
+    def min_principal_stress(self):
+        """min_principal_stress returns the min principal stress"""
+        return ""
+
+    def principal_angle(self):
+        """principal_angle returns the principle angle in degrees"""
+        return ""
+
+    def shear_angle(self):
+        """shear_angle returns the shear angle in degrees"""
+        return ""
