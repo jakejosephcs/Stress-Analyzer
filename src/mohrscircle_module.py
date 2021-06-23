c9f690dd46ed7c8a2f47974caceaa9bf462d2312
Jake Joseph <jakejosephcs@gmail.com>
2021-06-23 17:53:50 -0400

    Add plot function

--- src/mohrscircle_module.py
+++ src/mohrscircle_module.py
@@ -1,4 +1,6 @@
 import math
+import numpy
+import matplotlib.pyplot
 
 
 class MohrsCircle():
@@ -47,3 +49,47 @@ class MohrsCircle():
     def shear_angle(self):
         """shear_angle returns the shear angle in degrees"""
         return (90 - self.principal_angle()*2)/2
+
+    def plot_circle(self):
+        radians = numpy.linspace(0, 360, 361) * (2 * numpy.pi/360)
+        sigma_x = self.sigma_avg() + self.tau_max_xy() * numpy.cos(radians)
+        tau_y = self.tau_max_xy() * numpy.sin(radians)
+
+        matplotlib.pyplot.figure(figsize=[6, 6])
+        matplotlib.pyplot.plot(sigma_x, tau_y)
+        matplotlib.pyplot.plot(self.sigma_avg(), 0, 'ro')
+        matplotlib.pyplot.plot([self.sigma_x, self.sigma_y], [
+            self.tau_xy, -self.tau_xy], 'g')
+
+        matplotlib.pyplot.text(self.max_principal_stress(), 0.25,
+                               r'$\sigma_{max}$', va='bottom', ha='right', fontsize=12)
+        matplotlib.pyplot.text(self.min_principal_stress(), 0.25,
+                               r'$\sigma_{min}$', va='bottom', ha='left', fontsize=12)
+        matplotlib.pyplot.text(self.sigma_avg(), self.tau_max_xy(),
+                               r'$\tau_{max}$', va='top', ha='center', fontsize=12)
+        matplotlib.pyplot.text(self.sigma_avg(), 2,
+                               r'$\sigma_{avg}$', fontsize=12)
+
+        matplotlib.pyplot.title(
+            "Mohr's circle for two-dimensional state of stress")
+        matplotlib.pyplot.ylabel(r'$\tau$ (shear stess)')
+        matplotlib.pyplot.xlabel(r'$\sigma$ (normal stress)')
+        matplotlib.pyplot.axhline(c='k')
+        matplotlib.pyplot.axvline(c='k')
+        matplotlib.pyplot.grid()
+        matplotlib.pyplot.tight_layout()
+        matplotlib.pyplot.figtext(.85, .9,
+                                  "$\\theta_{}$ = {:.1f}°".format("p", self.principal_angle()))
+        matplotlib.pyplot.figtext(.85, .85,
+                                  "$\\theta_{}$ = {:.1f}°".format("s", self.shear_angle()))
+        matplotlib.pyplot.show()
+
+
+# x = MohrsCircle(50, -10, 40)
+# print(x.sigma_avg())
+# print(x.tau_max_xy())
+# print(x.max_principal_stress())
+# print(x.min_principal_stress())
+# print(x.principal_angle())
+# print(x.shear_angle())
+# x.plot_circle()
